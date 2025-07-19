eaa39b3dc495cb6b22493cb94e88f2c561f25a46
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-07-19 16:32:49 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './Pages/Home/Index'
+import Home from './Pages/Home/Home'
 
 
 function App() {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
 import "./Header.css"
+import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -2,
+    top: 2,
+    border: `3px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 const Header = () => {
     return (
@@ -16,15 +32,15 @@ const Header = () => {
                             </p>
                         </div>
                         <div className='cols2 '>
-                                <ul className='flex gap-[10px] bg-pink-600 justify-between items-center '>
+                                <ul className='flex gap-[2rem] justify-between items-center '>
                                     <li className='list-none'>
                                         <Link to="/help-center"
-                                        className='text-[12px] link font-[500] transition no-underline'
+                                        className='text-[14px] link font-[500] transition no-underline'
                                         >Help Center</Link>
                                     </li>
                                     <li className='list-none'>
                                         <Link to="/order-tracking"
-                                        className='text-[12px] link font-[500] transition no-underline'
+                                        className='text-[14px] link font-[500] transition no-underline'
                                         >Order Tracking</Link>
                                     </li>
                                     
@@ -35,25 +51,51 @@ const Header = () => {
             </div>
 
        
-            <div className='header container flex items-center justify-between'>
+            <div className='header'>
+            <div className='container flex items-center justify-between'>
 
-                <div className='col1 w-[30%]'>
+                <div className='header_col1 '>
                     <Link to="/">
-                    <img src={""} alt="logo"/>
+                    <img src={"https://serviceapi.spicezgold.com/download/1750047766437_logo.jpg"} alt="logo"/>
                     </Link>
                 </div>
 
-                <div className='col2 w-[40%]'>
+                <div className='header_col2 '>
                         <SearchBar/>
                 </div>
 
-                <div className='col3 w-[30%]'>
+                <div className='header_col3 '>
                     <div className='login-register'>
-                        
+                        <span><Link className='link' to="/login">Login |</Link></span>
+                       <span><Link  className='link' to="/register"> Register</Link></span>
+
+                    </div>
+
+                    <div className='wishList-cart'>
+                        <div className='wishList-cart-icon'>
+                            <Tooltip title="Wishlist" placement="bottom">
+                                <IconButton aria-label="wishlist">
+                                    <StyledBadge badgeContent={4} color="primary">
+                                        <FaRegHeart className='wishList-cart-button'/>
+                                    </StyledBadge>
+                                </IconButton>
+                            </Tooltip>
+                        </div>
+                        <div className='wishList-cart-icon'>
+                             <Tooltip title="Cart" placement="bottom">
+                            <IconButton aria-label="cart">
+                                <StyledBadge badgeContent={4} color="secondary">
+                                    <MdOutlineShoppingCart className='wishList-cart-button'/>
+                                </StyledBadge>
+                            </IconButton>
+                             </Tooltip>
+                        </div>
+                       
 
                     </div>
 
                 </div>
+            </div>
             </div>
 
           
