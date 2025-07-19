0e398a23807ba3abe0b77846e35c985614472912
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-07-19 16:29:53 +0530

    Added Home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Navbar from '../../components/NavBar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      Home
+    </div>
+  )
+}
+
+export default Home
