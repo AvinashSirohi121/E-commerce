2da96d1171167d7100a21a3c965d6124e86ad450
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-07-20 23:11:55 +0530

    new changes for slider, category slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
-import Navbar from '../../components/NavBar/Navbar'
-
+import Header from '../../components/HomePage_Components/Header/Header'
+import Navbar from "../../components/HomePage_Components/NavBar/Navbar"
+import Slider from '../../components/HomePage_Components/BannerSlider/Slider'
+import CategorySlider from '../../components/HomePage_Components/CategorySlider/CategorySlider'
+import "./Home.css"
 const Home = () => {
   return (
     <div>
       <Header/>
       <Navbar/>
+      <div className='home'>
+         <Slider/>
+      <CategorySlider/>
+      </div>
+     
       Home
     </div>
   )
