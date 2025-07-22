a6d97f788b5a4478e4d0c00bdc36e8d1506ce20e
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-07-23 00:25:55 +0530

    Added new compoenets

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,16 +3,24 @@ import Header from '../../components/HomePage_Components/Header/Header'
 import Navbar from "../../components/HomePage_Components/NavBar/Navbar"
 import Slider from '../../components/HomePage_Components/BannerSlider/Slider'
 import CategorySlider from '../../components/HomePage_Components/CategorySlider/CategorySlider'
+import AdsBannerSlider from '../../components/HomePage_Components/AdsBannerSlider/AdsBannerSlider'
+import AdsBannerSlider2 from '../../components/HomePage_Components/AdsBannerSlider/AdsBannerSlider2'
 import "./Home.css"
+import FreeShipping from '../../components/HomePage_Components/FreeShipping/FreeShipping'
 const Home = () => {
   return (
     <div>
       <Header/>
       <Navbar/>
       <div className='home'>
-         <Slider/>
-      <CategorySlider/>
+        <Slider/>
+        <CategorySlider className="py-8"/>
       </div>
+       <section className='middleSection'>
+        <FreeShipping/>
+        <AdsBannerSlider/>
+        <AdsBannerSlider2/>
+      </section>
      
       Home
     </div>
